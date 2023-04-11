bc683ee0d9f3c5ce46badb01c0f1d29c3e8d988f
Vipul Singh <vipul.singh@autodesk.com>
2023-04-11 16:09:41 +0530

    Making Changes to Reporting to Name the Report based on the Website

--- cypress.config.js
+++ cypress.config.js
@@ -15,8 +15,12 @@ module.exports = {
           lighthouse: lighthouse((lighthouseReport) =>
           {
             console.log("---- Writing lighthouse report to disk ----");
-
-            fs.writeFile("lighthouse.html", lighthouseReport.report, (error) =>
+            const dirPath = './PerfReports'
+                  if (!fs.existsSync(dirPath)) {
+                    fs.mkdirSync(dirPath)
+                  }
+            const name = (lighthouseReport.lhr.requestedUrl).replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, function (x) { return '' }) + " - " + (lighthouseReport.lhr.fetchTime).split('T')[0]
+            fs.writeFile(`${dirPath}/GLH-(${name}).html`, lighthouseReport.report, (error) =>
             {
               error ? console.log(error) : console.log("Report created successfully");
             });
